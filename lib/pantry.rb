0c6fbf1209d5ee15a10ac2374aa0b64d07b471af
NataliaColome <nmcolome@gmail.com>
2017-04-18 10:10:37 -0600

    finished #restock and #add_to_shopping_list functionality and tests, working on #print_shopping_list

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,8 +1,11 @@
+require './lib/recipe'
+
 class Pantry
-  attr_reader :stock
+  attr_reader :stock, :shopping_list
 
   def initialize
     @stock = {}
+    @shopping_list = {}
   end
 
   def stock_check(item)
@@ -11,10 +14,25 @@ class Pantry
   end
 
   def restock(item, amount)
-    if stock[item] != 0
+    if !stock[item].nil?
       stock[item] = stock[item] + amount
     else
       stock[item] = amount
     end
   end
+
+  def add_to_shopping_list(recipe)
+    recipe.ingredients.each do |item, amount|
+      if !shopping_list[item].nil?
+        shopping_list[item] = shopping_list[item] + amount
+      else
+        shopping_list[item] = amount
+      end
+    end
+  end
+
+  def print_shopping_list
+    
+  end
+
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -7,6 +7,14 @@ class PantryTest < Minitest::Test
 
   def setup
     @pantry = Pantry.new
+    @r = Recipe.new("Cheese Pizza")
+    @r.add_ingredient("Cheese", 20)
+    @r.add_ingredient("Flour", 20)
+
+    @r_2 = Recipe.new("Spaghetti")
+    @r_2.add_ingredient("Noodles", 10)
+    @r_2.add_ingredient("Sauce", 10)
+    @r_2.add_ingredient("Cheese", 5)
   end
 
   def test_it_exists
@@ -28,5 +36,28 @@ class PantryTest < Minitest::Test
 
     @pantry.restock("Cheese", 20)
     assert_equal 30, @pantry.stock_check("Cheese")
+  end
+
+  def test_generation_of_shopping_list_for_existing_recipes
+    @pantry.add_to_shopping_list(@r)
+
+    output = {"Cheese" => 20, "Flour" => 20}
+    assert_equal output, @pantry.shopping_list
+  end
+
+  def test_shopping_list_with_same_ingredients_for_different_recipes
+    @pantry.add_to_shopping_list(@r)
+    @pantry.add_to_shopping_list(@r_2)
+    
+    output = {"Cheese" => 25, "Flour" => 20, "Noodles" => 10, "Sauce" => 10}
+    assert_equal output, @pantry.shopping_list
+  end
+
+  def test_it_can_print_shopping_list
+    @pantry.add_to_shopping_list(@r)
+    @pantry.add_to_shopping_list(@r_2)
+    
+    output = "* Cheese: 20\n* Flour: 20\n* Spaghetti Noodles: 10\n* Marinara Sauce: 10"
+    assert_equal output = @pantry.print_shopping_list
   end
 end
